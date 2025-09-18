a323d1a37aa5e0275329e59b0eacb00a6a66df9b
MercyAkpan <akpanmeme@gmail.com>
2025-09-18 21:43:20 +0100

    Added the factory file

---
+++ Factory/CompositeFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DesignPatternProject.Interfaces;
+
+namespace DesignPatternProject.Factory
+{
+    /// <summary>
+    /// This Factory Interface contains methods to create instances of nodes in the Composite tree.
+    /// </summary>
+    /// <remarks>
+    /// It is an Interface and not an abstract class because of varying logic in implementators.
+    /// </remarks>
+    internal interface IComponentFactory
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        IProjectElement CreatePhase(string name);
+        IProjectElement CreateTask(string name, decimal cost);
+        IProjectElement CreateStakeholder(string type, decimal budget, bool hasPermit, string permitNumber);
+
+    }
+}
