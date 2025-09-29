96fc5543ac474b75806df0b949ae238ef39b7d56
MercyAkpan <akpanmeme@gmail.com>
2025-09-29 22:39:46 +0100

    Added customised interfaces for type safe access to all nodes in the tree. Trades off on 'transparency' though.

--- Interfaces/IProjectElement.cs
+++ Interfaces/IProjectElement.cs
@@ -5,13 +5,45 @@ using System.Linq;
 
 namespace DesignPatternProject.Interfaces
 {
+    /// <summary>
+    /// This interface contains common behavior for all elements in the Composite tree.
+    /// </summary>
     public interface IProjectElement
     {
-        // Read-only properties
         string Name { get; }
+        /// <summary>
+        /// This is the unique id of the Stakeholder.
+        /// </summary>
+        string Id { get; }
+        /// <summary>
+        /// Gets the cost of the project element.   
+        /// </summary>
+        /// <returns>
+        /// The cost of the project element.
+        /// </returns>
         decimal GetCost();
         string GetDescription();
         void DisplayInfo(int depth = 0);
         IEnumerator<IProjectElement> CreateIterator();
     }
+    ///<summary>
+    /// This interface contains leaf-specific behavior in the Composite tree.
+    /// </summary>
+    public interface IProjectUnit : IProjectElement
+    {
+        // Leaf-specific methods would be added here.
+    }
+    ///<summary>
+    /// This interface contains composite-specific behavior in the Composite tree.
+    ///</summary>
+    public interface IProjectPhase : IProjectElement
+    {
+        /// <summary>
+        /// This adds an element to the Composite tree.
+        /// </summary>
+        /// <param name="component"></param>
+        void AddComponent(IProjectElement component);
+        List<IProjectElement> GetComponents();
+    }
+
 }
